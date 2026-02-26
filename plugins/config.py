d0de0eb1b768bb38be4bab70ec465415b9fc24da
SujanCh740 <132272276+SujanCh740@users.noreply.github.com>
2026-02-26 07:28:28 +0545

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -16,13 +16,12 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "")
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-    MAX_FILE_SIZE = 2194304000
-    TG_MAX_FILE_SIZE = 2194304000
-    FREE_USER_MAX_FILE_SIZE = 2194304000
+    MAX_FILE_SIZE = 11194304000
+    TG_MAX_FILE_SIZE = 4100000000
+    FREE_USER_MAX_FILE_SIZE = 11194304000
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://via.placeholder.com/320x320.png?text=Thumbnail")
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
-    
     OUO_IO_API_KEY = ""
     MAX_MESSAGE_LENGTH = 4096
     PROCESS_MAX_TIMEOUT = 3600
